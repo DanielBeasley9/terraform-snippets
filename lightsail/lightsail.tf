05f2668f21be780a201dfeba41c94fe063260705
DanB1998 <danbeasley1998@gmail.com>
2025-01-29 20:54:31 +0000

    Lightsail tf file with budget and gitignore

--- lightsail/lightsail.tf
+++ lightsail/lightsail.tf
@@ -2,10 +2,26 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_budgets_budget" "lightsail_budget" {
+  name              = "budget-lightsail-monthly"
+  budget_type       = "COST"
+  limit_amount      = "15"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    subscriber_email_addresses = ["danbeasley1998@gmail.com"]
+    notification_type          = "FORECASTED"
+  }
+}
+
 # Lightsail WordPress Instance
 resource "aws_lightsail_instance" "wordpress" {
   name              = "wordpress-instance"
-  availability_zone = "eu-west-2"
+  availability_zone = "eu-west-2a"
   blueprint_id      = "wordpress"
   bundle_id         = "micro_1_0"
 
